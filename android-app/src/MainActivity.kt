7b60e787546fa047f31e716c6867694ea70c2123
Jozsef Szilvasi <jozsef.szilvasi@gmail.com>
2025-05-22 22:40:25 +0300

    Introduce better dependency handling

--- android-app/src/MainActivity.kt
+++ android-app/src/MainActivity.kt
@@ -2,12 +2,12 @@ package com.naturlecso.mtgbooster
 
 import Screen
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import com.arkivanov.decompose.defaultComponentContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
